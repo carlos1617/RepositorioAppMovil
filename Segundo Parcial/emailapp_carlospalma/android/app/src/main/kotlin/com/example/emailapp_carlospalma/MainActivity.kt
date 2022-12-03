c7a324c407b0ac71983359be07a91baabdc1d60b
carlos1617 <cepm1315475358@gmail.com>
2022-12-02 21:04:19 -0500

    EmailApp

---
+++ Segundo Parcial/emailapp_carlospalma/android/app/src/main/kotlin/com/example/emailapp_carlospalma/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.emailapp_carlospalma
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Segundo Parcial/emailapp_carlospalma/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/list_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Correo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xff9b9b9b),
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const ListScreen(title: 'Outlook | Carlos Palma'),
+    );
+  }
+}
